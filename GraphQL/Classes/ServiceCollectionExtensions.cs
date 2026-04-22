fad2f97b3d142cb85aa45af9f064142de091fcf7
Martin Almeida <MA338947@fi365.ort.edu.uy>
2026-04-22 17:40:14 -0300

    Extraida configuración de servicios a static class ServiceCollectionExtensions, cambio de retorno Environment.Error(1) a ArgumentNullException en caso de connectionString vacia

---
+++ GraphQL/Classes/ServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Domain.Interfaces.Public.Repositories;
+using Domain.Enums;
+using DataAccess.EntityFramework;
+using DataAccess.Repositories;
+using Application.DTOs.Users;
+using Application.Interfaces.Public.Services;
+using Application.Services;
+using GraphQL.Schema;
+
+namespace GraphQL.Classes;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        string connectionString = configuration.GetConnectionString("DefaultConnection")
+        ?? throw new ArgumentNullException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(
+            connectionString, options => options
+                .MapEnum<Domain.Enums.UserState>("user_state_enum")
+                .MapEnum<BillType>("bill_type_enum")
+                .MapEnum<MembershipState>("membership_state_enum")));
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserService, UserService>();
+        return services;
+    }
+
+    public static IServiceCollection AddServiceGraphQL(this IServiceCollection services)
+    {
+        services.AddGraphQLServer()
+        .AddType<ResultUserDTO>()
+        .AddType<ResultBroadcasterDTO>()
+        .AddType<ResultClientDTO>()
+        .AddInterfaceType<IResultUserDTO>()
+        .AddQueryType<Query>()
+        .AddMutationType<Mutation>()
+        .AddProjections()             // Optimizes SQL queries
+        .AddFiltering()               // Allow users to filter results
+        .AddSorting()                 // Allow users to sort results
+        .ModifyRequestOptions(opt => opt.IncludeExceptionDetails = true);
+        return services;
+    }
+}
--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,67 +1,12 @@
-using Microsoft.EntityFrameworkCore;
-using Domain.Interfaces.Public.Repositories;
-using Application.Interfaces.Public.Services;
-using DataAccess.EntityFramework;
-using Application.Services;
-using Domain.Enums;
-using GraphQL.Schema;
-using DataAccess.Repositories;
-using Application.DTOs.Users;
+using GraphQL.Classes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Standard DI registrations
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IClientRepository, ClientRepository>();
-builder.Services.AddScoped<IBroadcasterRepository, BroadcasterRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ICountryRepository, CountryRepository>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-// builder.Services.AddScoped<IContractService, ContractService>();
-// builder.Services.AddScoped<IBillService, BillService>();
+builder.Services.AddApplicationServices();
 
-string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDatabase(builder.Configuration);
 
-if (connectionString == null)
-{
-    Console.WriteLine("Connection string not found.");
-    Environment.Exit(1);
-}
-
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(
-    connectionString, options => options
-        .MapEnum<Domain.Enums.UserState>("user_state_enum")
-        .MapEnum<BillType>("bill_type_enum")
-        .MapEnum<MembershipState>("membership_state_enum")));
-
-// NpgsqlDataSourceBuilder dataSourceBuilder = new(connectionString)
-// {
-//     DefaultNameTranslator = new NpgsqlSnakeCaseNameTranslator()
-// };
-// dataSourceBuilder.MapEnum<Domain.Enums.UserState>("user_state_enum"); // Name must match PG exactly
-// dataSourceBuilder.MapEnum<BillType>("bill_type_enum"); // Name must match PG exactly
-// dataSourceBuilder.MapEnum<MembershipState>("membership_state_enum"); // Name must match PG exactly
-// NpgsqlDataSource dataSource = dataSourceBuilder.Build();
-
-// builder.Services.AddDbContext<DatabaseContext>(options =>
-//     options.UseNpgsql(dataSource)
-//     .LogTo(Console.WriteLine, LogLevel.Information)
-//     .EnableSensitiveDataLogging()
-// );
-
-builder.Services
-    // .AddSingleton(dataSource)
-    .AddGraphQLServer()
-    .AddQueryType<Query>()        // We'll create this class next
-    .AddType<ResultUserDTO>()
-    .AddType<ResultBroadcasterDTO>()
-    .AddType<ResultClientDTO>()
-    .AddInterfaceType<IResultUserDTO>()
-    .AddMutationType<Mutation>()
-    .AddProjections()             // Optimizes SQL queries
-    .AddFiltering()               // Allow users to filter results
-    .AddSorting()                // Allow users to sort results
-    .ModifyRequestOptions(opt => opt.IncludeExceptionDetails = true);
+builder.Services.AddServiceGraphQL();
 
 var app = builder.Build();
 
