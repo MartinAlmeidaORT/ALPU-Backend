813e5a308d2ea938f11565bfbedb2d761a06d64e
Martin Almeida <MA338947@fi365.ort.edu.uy>
2026-05-11 14:51:36 -0300

    fix: add IDepartmentRepository to UnitOfWork

--- DataAccess/Repositories/UnitOfWork.cs
+++ DataAccess/Repositories/UnitOfWork.cs
@@ -11,6 +11,7 @@ public class UnitOfWork(DatabaseContext context) : IUnitOfWork
     private IBroadcasterRepository? _broadcasters;
     private IClientRepository? _clients;
     private ICountryRepository? _countries;
+    private IDepartmentRepository? _departments;
     private IAlpuServiceRepository? _services;
     private IContractRepository? _contracts;
 
@@ -18,6 +19,7 @@ public class UnitOfWork(DatabaseContext context) : IUnitOfWork
     public IBroadcasterRepository Broadcasters => _broadcasters ??= new BroadcasterRepository(_context);
     public IClientRepository Clients => _clients ??= new ClientRepository(_context);
     public ICountryRepository Countries => _countries ??= new CountryRepository(_context);
+    public IDepartmentRepository Departments => _departments ??= new DepartmentRepository(_context);
     public IAlpuServiceRepository Services => _services ??= new AlpuServiceRepository(_context);
     public IContractRepository Contracts => _contracts ??= new ContractRepository(_context);
 
