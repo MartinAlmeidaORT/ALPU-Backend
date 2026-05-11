c8226a324c194e8b13b93ee5f057201161496011
TadeoMieres <YM332576@fi365.ort.edu.uy>
2026-05-11 14:35:18 -0300

    hotfix in calculateContratPayload apiDeleted

--- Application/Interfaces/Public/Services/IContractService.cs
+++ Application/Interfaces/Public/Services/IContractService.cs
@@ -7,7 +7,7 @@ namespace Application.Interfaces.Public.Services;
 
 public interface IContractService
 {
-    Task<ResultAPI<CalculateContractPayload>> CalculateContract(CalculateContractInput input);
+    Task<Result<CalculateContractPayload>> CalculateContract(CalculateContractInput input);
     IQueryable<Contract> GetAllContracts();
     Task<Contract?> GetContractByIdAsync(int id);
     Task<Contract> CreateContractAsync(Contract contract);
