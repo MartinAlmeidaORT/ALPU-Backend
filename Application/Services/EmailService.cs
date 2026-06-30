faf43677af23e5e5d7c6417be80c9bf35e78b6ae
TadeoMieres <YM332576@fi365.ort.edu.uy>
2026-06-30 17:16:32 -0300

    email sender fixed

--- Application/Services/EmailService.cs
+++ Application/Services/EmailService.cs
@@ -9,11 +9,13 @@ namespace Application.Services;
 public class EmailService : IEmailService
 {
     private readonly IConfiguration _config;
+    
 
     public EmailService(IConfiguration config)
     {
         _config = config;
     }
+
 
     public Task SendAccountPendingAsync(string toEmail, string userName) =>
         SendAsync(
