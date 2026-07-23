62097e1aabf8e3e53ea26352bf87eac0de756eaf
TadeoMieres <YM332576@fi365.ort.edu.uy>
2026-07-22 21:50:17 -0300

    .webp extension added

--- DataAccess/ExternalServices/AmazonS3Service.cs
+++ DataAccess/ExternalServices/AmazonS3Service.cs
@@ -11,7 +11,7 @@ public class AmazonS3Service(IAmazonS3 s3Client, IConfiguration config)
     private readonly IAmazonS3 _s3Client = s3Client;
     private readonly string _bucketName = config["AWS:BucketName"]!;
 
-    private static readonly string[] ImageExtensions = { ".jpg", ".jpeg", ".png" };
+    private static readonly string[] ImageExtensions = { ".jpg", ".jpeg", ".png", ".webp"};
     private static readonly string[] AudioExtensions = { ".mp3", ".wav", ".m4a", ".ogg" };
 
     public async Task<Result<(string, string)>> SaveBillProofAsync(string fileName, BillType type)
@@ -152,6 +152,7 @@ public class AmazonS3Service(IAmazonS3 s3Client, IConfiguration config)
     private static string GetContentType(string extension) => extension switch
     {
         ".jpg" or ".jpeg" => "image/jpeg",
+        ".webp" => "image/webp",
         ".png" => "image/png",
         ".mp3" => "audio/mpeg",
         ".wav" => "audio/wav",
