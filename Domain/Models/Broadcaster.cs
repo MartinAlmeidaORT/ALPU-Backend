2644d09ba22486f02f11eec4366b062afcb150e6
MartinAlmeidaORT <MA338947@fi365.ort.edu.uy>
2026-07-24 16:02:52 -0300

    add broadcaster profile update validation

--- Domain/Models/Broadcaster.cs
+++ Domain/Models/Broadcaster.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+using Domain.Common.Errors;
 using Domain.Common.Inputs;
 using Domain.Common.Inputs.Auth;
 using FluentResults;
@@ -42,6 +44,9 @@ public class Broadcaster : User
     public int CategoryId { get; set; }
 
     public BroadcasterCategory Category { get; set; } = null!;
+
+    [GeneratedRegex(@"^\+?[0-9]+$")]
+    private static partial Regex PhoneRegex();
 
     public string? PhoneNumber { get; set; }
 
@@ -94,7 +99,75 @@ public class Broadcaster : User
         return base.ValidateUpdate();
     }
 
+    public Result ValidatePhoneNumber()
+    {
+        if (PhoneNumber == null) return Result.Ok();
+
+        if (PhoneNumber.Length > 15)
+        {
+            return Result.Fail(BroadcasterErrors.PhoneNumberMaxLength());
+        }
+
+        if (!PhoneRegex().IsMatch(PhoneNumber))
+        {
+            return Result.Fail(BroadcasterErrors.InvalidNumberFormat());
+        }
+
+        return Result.Ok();
+    }
+
+    public Result ValidateWebsite()
+    {
+        if (Website == null) return Result.Ok();
+
+        if (!Uri.TryCreate(Website, UriKind.Absolute, out Uri? uriResult) ||
+            (uriResult.Scheme != Uri.UriSchemeHttp && uriResult.Scheme != Uri.UriSchemeHttps))
+        {
+            return Result.Fail(BroadcasterErrors.InvalidWebsite());
+        }
+
+        return Result.Ok();
+    }
+
+    public Result ValidateDescription()
+    {
+        if (Description == null) return Result.Ok();
+
+        if (Description.Length < 50)
+        {
+            return Result.Fail(BroadcasterErrors.DescriptionMinLength());
+        }
+
+        if (Description.Length > 250)
+        {
+            return Result.Fail(BroadcasterErrors.DescriptionMinLength());
+        }
+
+        return Result.Ok();
+    }
+
     public void UpdateSkills(ICollection<Skill> skills) => Skills = skills;
 
     public void UpdateLanguages(ICollection<Language> languages) => Languages = languages;
+}
+
+public static class BroadcasterErrors
+{
+    // Errors class
+    public class PhoneNumberLengthError(string msg) : ValidationError(msg);
+    public class PhoneNumbersOnlyError(string msg) : ValidationError(msg);
+
+    public class InvalidWebsiteError(string msg) : ValidationError(msg);
+
+    public class DescriptionMinLengthError(string msg) : ValidationError(msg);
+    public class DescriptionMaxLengthError(string msg) : ValidationError(msg);
+
+    // Factory Methods
+    public static PhoneNumberLengthError PhoneNumberMaxLength() => new("El telefono debe tener hasta 15 caracteres.");
+    public static PhoneNumbersOnlyError InvalidNumberFormat() => new("El telefono solo puede tener numeros.");
+
+    public static InvalidWebsiteError InvalidWebsite() => new("El sitio web no es valido.");
+
+    public static DescriptionMinLengthError DescriptionMinLength() => new("La description debe tener al menos 50 caracteres.");
+    public static DescriptionMaxLengthError DescriptionMaxLength() => new("La descripcion puede tener hasta 250 caracteres.");
 }
